fe1165ffde90fa92fa337d981ae088973bc481ae
Alex-Magdich <alexkotorij@gmail.com>
2018-10-02 21:05:49 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about/:id', component: AboutComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
+  [ RouterModule.forRoot(routes) ],
     BrowserModule
   ],
   providers: [],
